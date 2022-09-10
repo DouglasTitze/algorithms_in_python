e628b9e791448f186e040d410fb606e46619d428
Douglas Titze <douglasmarcost2@gmail.com>
2022-09-09 21:32:11 -0400

    Formatted File

--- two_sum.py
+++ two_sum.py
@@ -5,12 +5,11 @@ from itertools import permutations
 
 def two_sum(arr, target):
 
-    #All permutations
-    #for i in range(len(arr)):
+    # All permutations
+    # for i in range(len(arr)):
     #    for j in range(len(arr)):
     #        if (arr[i] + arr[j]) == target and i != j:
     #            print(i,j)
-    
 
     # All combinations
     return (
@@ -20,7 +19,8 @@ def two_sum(arr, target):
         if arr[i] + arr[j] == target
     )
 
-def two_sum_hash_tables(arr: array,target: int) -> None:
+
+def two_sum_hash_tables(arr: array, target: int) -> None:
     dic = {}
 
     for index, num in enumerate(arr):
@@ -28,7 +28,7 @@ def two_sum_hash_tables(arr: array,target: int) -> None:
 
         if left_over in dic:
             print((index, dic[left_over]))
-        
+
         else:
             dic[num] = index
 
